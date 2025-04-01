576828b04349256ee6bc214adab766943dfbdbfb
Xylight0 <greppn@gmail.com>
2025-04-01 18:25:53 +0200

    Feature: add compileStmt

--- src/compilers/lang_var/var_compiler.py
+++ src/compilers/lang_var/var_compiler.py
@@ -2,7 +2,9 @@ from lang_var.var_ast import *
 from common.wasm import *
 import lang_var.var_tychecker as var_tychecker
 from common.compilerSupport import *
-import common.utils as utils
+
+# import common.utils as utils
+
 
 def compileModule(m: mod, cfg: CompilerConfig) -> WasmModule:
     """
@@ -10,14 +12,50 @@ def compileModule(m: mod, cfg: CompilerConfig) -> WasmModule:
     """
     vars = var_tychecker.tycheckModule(m)
     instrs = compileStmts(m.stmts)
-    idMain = WasmId('$main')
-    locals: list[tuple[WasmId, WasmValtype]] = [(identToWasmId(x), 'i64') for x in vars]
-    return WasmModule(imports=wasmImports(cfg.maxMemSize),
-    exports=[WasmExport("main", WasmExportFunc(idMain))],
-    globals=[],
-    data=[],
-    funcTable=WasmFuncTable([]),
-    funcs=[WasmFunc(idMain, [], None, locals, instrs)])
-    
-def compileStmts(stmts: list[stmt]) -> list[WasmInstr]: 
-    
+    idMain = WasmId("$main")
+    locals: list[tuple[WasmId, WasmValtype]] = [(identToWasmId(x), "i64") for x in vars]
+    return WasmModule(
+        imports=wasmImports(cfg.maxMemSize),
+        exports=[WasmExport("main", WasmExportFunc(idMain))],
+        globals=[],
+        data=[],
+        funcTable=WasmFuncTable([]),
+        funcs=[WasmFunc(idMain, [], None, locals, instrs)],
+    )
+
+
+def identToWasmId(x: Ident) -> WasmId:
+    return WasmId(f"${x.name}")
+
+
+def compileStmts(stmts: list[stmt]) -> list[WasmInstr]:
+    instructions: list[WasmInstr] = []
+
+    for stmt in stmts:
+        instructions.append(compileStmt(stmt))
+
+    return instructions
+
+
+def compileStmt(stmt: stmt) -> WasmInstr:
+
+    match stmt:
+        case Assign(x,IntConst(n)):
+            print(f"Assign: {x} = {n}")
+            instruction = WasmInstrComment(f"Assign: {x} = {n}")
+        case Assign(_,_):
+            print(f"Assign")
+            instruction = WasmInstrComment(f"Assign")
+        case StmtExp(e):
+            print(f"StmtExp: {x} = {n}")
+            instruction = Stm
+        
+
+    return instruction
+
+    # for statement in stmts:
+    #     if isinstance(statement, Assign):
+    #         left = statement.var
+    #         right = statement.right
+    #         a = Assign(left, right)
+    #         print(a.var)
